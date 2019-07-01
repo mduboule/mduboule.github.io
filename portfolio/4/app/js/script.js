e29cecad1ca6f1166fd731e7ed29a7877b8b6e66
Marius Duboule <mduboule@gmail.com>
2019-07-01 18:45:15 -0400

    Mobile version done.
    Features a dynamic menu, features section, FAQ, header/footer.
    
    Added fonts (light/regular/medium).
    
    SVG styling through JS and eventsListeners.

--- portfolio/4/app/js/script.js
+++ portfolio/4/app/js/script.js
@@ -1,14 +1,14 @@
-var nav = document.querySelector('#main-nav');
-var socials = document.querySelector('#socials');
-var menu = document.querySelector('#menu');
-var navToggle = document.querySelector('#nav-toggle');
-var logo = document.querySelector('#logo');
-var body = document.querySelector('body');
+const nav = document.querySelector('#main-nav');
+const socials = document.querySelector('#socials');
+const menu = document.querySelector('#menu');
+const navToggle = document.querySelector('#nav-toggle');
+const logo = document.querySelector('#logo');
+const body = document.querySelector('body');
 
 function toggle() {
     /* Toggles menu, nav and social icons */
-    nav.classList.toggle('hide');
-    socials.classList.toggle('hide');
+    nav.classList.toggle('hidden-nav');
+    socials.classList.toggle('hidden-socials');
     menu.classList.toggle('hidden-menu');
 
     /* Change the toggle style */
@@ -20,9 +20,7 @@ function toggle() {
     toggleLogo();
 
     /* Prevent from scrolling */
-    body.classList.toggle('stop-scrolling');
-    // logo.getSVGDocument().querySelectorAll().setAttribute("fill", "white");
-    // logo.getSVGDocument().querySelectorAll().setAttribute("stroke", "white");
+    // body.classList.toggle('stop-scrolling');
 }
 
 function toggleLogo() {
@@ -34,5 +32,44 @@ function toggleLogo() {
         logo.getSVGDocument().querySelector('#text').setAttribute('fill', '#242A45');
         logo.getSVGDocument().querySelector('#circle').setAttribute('fill', '#5267DF');
         logo.getSVGDocument().querySelector('#flag').setAttribute('opacity', '#FFF');
+    }
+}
+
+function toggleTab(tab) {
+    const tabToToggle = tab.getAttribute("data-tab-target");
+    const features = document.querySelectorAll('section.features .feature');
+    const links = document.querySelectorAll('nav.features button');
+
+    features.forEach((feature) => {
+        if (feature.getAttribute('data-tab') === tabToToggle) {
+            feature.classList.remove('hide');
+        } else {
+            feature.classList.add('hide');
+        }
+    });
+
+    links.forEach((link) => {
+        if (link.getAttribute('data-tab-target') === tabToToggle) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+}
+
+function toggleAnswer(question) {
+    let questionNumber = question.getAttribute("data-question");
+    let answer = document.querySelector(`#answer${questionNumber}`);
+    let arrow = document.querySelector(`#arrow-q${questionNumber}`);
+
+    let arrowFill = arrow.getSVGDocument().querySelector('path').getAttribute('stroke');
+
+    answer.classList.toggle('hidden-answer');
+    arrow.classList.toggle('opened');
+
+    if (arrowFill !== '#fa5757') {
+        arrow.getSVGDocument().querySelector('path').setAttribute('stroke', '#fa5757');
+    } else {
+        arrow.getSVGDocument().querySelector('path').setAttribute('stroke', 'rgb(83, 104, 223)');
     }
 }
--- portfolio/4/dist/all.js
+++ portfolio/4/dist/all.js
@@ -1,1 +1,75 @@
-var nav=document.querySelector("#main-nav"),socials=document.querySelector("#socials"),menu=document.querySelector("#menu"),navToggle=document.querySelector("#nav-toggle"),logo=document.querySelector("#logo"),body=document.querySelector("body");function toggle(){nav.classList.toggle("hide"),socials.classList.toggle("hide"),menu.classList.toggle("hidden-menu"),navToggle.classList.toggle("active"),navToggle.classList.toggle("fa-bars"),navToggle.classList.toggle("fa-times"),toggleLogo(),body.classList.toggle("stop-scrolling")}function toggleLogo(){"#242A45"===logo.getSVGDocument().querySelector("#text").getAttribute("fill")?(logo.getSVGDocument().querySelector("#text").setAttribute("fill","white"),logo.getSVGDocument().querySelector("#circle").setAttribute("fill","white"),logo.getSVGDocument().querySelector("#flag").setAttribute("fill","#242A45")):(logo.getSVGDocument().querySelector("#text").setAttribute("fill","#242A45"),logo.getSVGDocument().querySelector("#circle").setAttribute("fill","#5267DF"),logo.getSVGDocument().querySelector("#flag").setAttribute("opacity","#FFF"))}
+const nav = document.querySelector('#main-nav');
+const socials = document.querySelector('#socials');
+const menu = document.querySelector('#menu');
+const navToggle = document.querySelector('#nav-toggle');
+const logo = document.querySelector('#logo');
+const body = document.querySelector('body');
+
+function toggle() {
+    /* Toggles menu, nav and social icons */
+    nav.classList.toggle('hidden-nav');
+    socials.classList.toggle('hidden-socials');
+    menu.classList.toggle('hidden-menu');
+
+    /* Change the toggle style */
+    navToggle.classList.toggle('active');
+    navToggle.classList.toggle('fa-bars');
+    navToggle.classList.toggle('fa-times');
+
+    /* Change logo colors */
+    toggleLogo();
+
+    /* Prevent from scrolling */
+    // body.classList.toggle('stop-scrolling');
+}
+
+function toggleLogo() {
+    if (logo.getSVGDocument().querySelector('#text').getAttribute('fill') === "#242A45") {
+        logo.getSVGDocument().querySelector('#text').setAttribute('fill', 'white');
+        logo.getSVGDocument().querySelector('#circle').setAttribute('fill', 'white');
+        logo.getSVGDocument().querySelector('#flag').setAttribute('fill', '#242A45');
+    } else {
+        logo.getSVGDocument().querySelector('#text').setAttribute('fill', '#242A45');
+        logo.getSVGDocument().querySelector('#circle').setAttribute('fill', '#5267DF');
+        logo.getSVGDocument().querySelector('#flag').setAttribute('opacity', '#FFF');
+    }
+}
+
+function toggleTab(tab) {
+    const tabToToggle = tab.getAttribute("data-tab-target");
+    const features = document.querySelectorAll('section.features .feature');
+    const links = document.querySelectorAll('nav.features button');
+
+    features.forEach((feature) => {
+        if (feature.getAttribute('data-tab') === tabToToggle) {
+            feature.classList.remove('hide');
+        } else {
+            feature.classList.add('hide');
+        }
+    });
+
+    links.forEach((link) => {
+        if (link.getAttribute('data-tab-target') === tabToToggle) {
+            link.classList.add('active');
+        } else {
+            link.classList.remove('active');
+        }
+    });
+}
+
+function toggleAnswer(question) {
+    let questionNumber = question.getAttribute("data-question");
+    let answer = document.querySelector(`#answer${questionNumber}`);
+    let arrow = document.querySelector(`#arrow-q${questionNumber}`);
+
+    let arrowFill = arrow.getSVGDocument().querySelector('path').getAttribute('stroke');
+
+    answer.classList.toggle('hidden-answer');
+    arrow.classList.toggle('opened');
+
+    if (arrowFill !== '#fa5757') {
+        arrow.getSVGDocument().querySelector('path').setAttribute('stroke', '#fa5757');
+    } else {
+        arrow.getSVGDocument().querySelector('path').setAttribute('stroke', 'rgb(83, 104, 223)');
+    }
+}
